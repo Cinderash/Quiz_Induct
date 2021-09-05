b3a30d8f9776e4aed999925719261ca7edc562cc
Cinderash <neha.panchariya.6112@gmail.com>
2021-09-05 23:31:07 +0530

    Quiz_Induct

---
+++ js/questions.js
@@ -0,0 +1,60 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "How did Zinetsu's hair color change?",
+    answer: "Struck by lightning",
+    options: [
+      "Struck by lightning.",
+      "Ate too many lemon.",
+      "Demon cursed him. ",
+      "Dyed it for attention."
+      
+    ]
+  },
+    {
+    numb: 2,
+    question: "What can kill a demon?",
+    answer: "Both are correct!",
+    options: [
+      "Nichirin blade forged from a unique ore that absorbs sunlight.",
+      "Wisteria-poison coated weapon.",
+      "Both are correct!",
+      "Both are wrong!"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Name of Tanjiro's Kasugai crows ?",
+    answer: "Matsuemon tennoji",
+    options: [
+      "Ukugi",
+      "Aleister Crowley",
+      "Matsuemon tennoji",
+      "Chuntaro."
+    ]
+  },
+    {
+    numb: 4,
+    question: "Correct name for Shinobu Kocho's Breathing Styles?",
+    answer: "Insect Breathing",
+    options: [
+      "Pest Breathing",
+      "Wisteria Breathing",
+      "Poison Breathing",
+      "Insect Breathing"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What color is Tanjiro Kamado's blade?",
+    answer: "Black",
+    options: [
+      "Red",
+      "Black",
+      "Blue",
+      "Grey"
+    ]
+  }
+  
+];
